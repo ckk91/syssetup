7a9c4c8d453f9e7a1890052f64c1753c72a8cb2f
Chris <12696988+ckk91@users.noreply.github.com>
2021-07-25 00:55:06 +0200

    Update bootstrap_wsl.ps1

--- win10/bootstrap_wsl.ps1
+++ win10/bootstrap_wsl.ps1
@@ -2,16 +2,13 @@
 # enable wsl2
 function enable-wsl {
     $ErrorActionPreference = "Stop"
+    
+    Write-Host Enabling WSL2 and restarting the machine.
+    
     dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
     dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
-
-    # make ready for rerun
-    push-location HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnce
-    new-itemproperty . MyKey -propertytype String -value "Start-Process powershell -Wait -Verb runAs -ArgumentList ' c:\bootstrap_wsl.ps1 setup'"
-    pop-location
-
-    Copy-Item .\bootstrap_wsl.ps1 -Destination C:\bootstrap_wsl.ps1
-    # todo cleanup
+    
+    # todo make reboot safe as admin
 
     Restart-Computer
 }
@@ -19,23 +16,34 @@ function enable-wsl {
 function initialize-ubuntu {
     $ProgressPreference = 'SilentlyContinue'
     $ErrorActionPreference = "Stop"
+    
+    Write-Host Fetching and applying WSL2 update
+    
     Invoke-WebRequest -Uri https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -OutFile .\wsl_update.msi
     # omfg https://powershellexplained.com/2016-10-21-powershell-installing-msi-files/
     Start-Process msiexec.exe -Wait -ArgumentList ' /I .\wsl_update.msi /quiet'
     Remove-Item '.\wsl_update.msi'
 
-     wsl --set-default-version 2
+    Write-Host Setting WSL Default Version 2
+    wsl --set-default-version 2
 
+    Write-Host Installing Ubuntu 20.04 LTS
     Invoke-WebRequest -Uri https://aka.ms/wslubuntu2004 -OutFile Ubuntu.appx -UseBasicParsing
     Add-AppxPackage .\Ubuntu.appx
     Remove-Item '.\Ubuntu.appx'
 
     # Initial housekeeping
     #https://github.com/kaisalmen/wsltooling/blob/world/installUbuntuLTS.ps1
-     wsl -u root -- apt update `&`& apt upgrade -y
+    Write-Host Initial Ubuntu update
+    wsl -u root -- apt update `&`& apt upgrade -y
 
-     $ProgressPreference = 'Continue'  # because ms engineers cant do concurrency apparently
-# https://stackoverflow.com/a/15166968
-# todo https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi
-# after reboot
+    $ProgressPreference = 'Continue'  # because ms engineers cant do concurrency apparently
+    # https://stackoverflow.com/a/15166968
+
+}
+
+if ($args[0] -eq "setup") {
+    initialize-ubuntu
+} else {
+    enable-wsl
 }
