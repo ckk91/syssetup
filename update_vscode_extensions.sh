aee5046da78f69b7f2077495bfafd9008e9de20b
Chris K <hello@chris-k.at>
2021-07-25 13:18:21 +0200

    cleanup

--- update_vscode_extensions.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-code --list-extensions >> vscode_extensions.list
-sort vscode_extensions.list | uniq > vscode_extensions.list.tmp
-mv vscode_extensions.list.tmp vscode_extensions.list
